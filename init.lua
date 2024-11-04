4f5551a342f143ae48f012c8949cc755d54c2020
Joshua Krieger <peremptor1@gmx.de>
2024-11-04 16:52:03 +0100

    - add navigation keybinds for windows - add neotree - add some settings for the editor view

--- init.lua
+++ init.lua
@@ -16,15 +16,49 @@ end
 print('init loaded')
 vim.opt.rtp:prepend(lazypath)
 
-
+-- Vim editor settings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes' 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.scrolloff = 7
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- split window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+--- end vim editor settings
 
 local plugins = {
 	{"catppuccin/nvim", name = "catppuccin", priority = 1000},
-	{"nvim-telescope/telescope.nvim", dependencies={"nvim-lua/plenary.nvim"} }
+	{"nvim-telescope/telescope.nvim", dependencies={"nvim-lua/plenary.nvim"} },
+	{
+    		"nvim-neo-tree/neo-tree.nvim",
+    		branch = "v3.x",
+    		dependencies = 
+		{
+      			"nvim-lua/plenary.nvim",
+      			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      			"MunifTanjim/nui.nvim",
+      			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	}
 }
 local opts = {}
 
@@ -35,3 +69,5 @@ vim.cmd.colorscheme "catppuccin-mocha"
 
 local telescopeHandle = require("telescope.builtin")
 vim.keymap.set("n", "<leader><leader>", telescopeHandle.find_files, {})
+
+vim.keymap.set("n", "\\", ":Neotree filesystem reveal left<CR>")
