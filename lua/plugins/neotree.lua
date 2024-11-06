a499758d1d847a972f88e271b81af916b4a5f50b
Joshua Krieger <peremptor1@gmx.de>
2024-11-06 22:44:52 +0100

    - half page scrolling now also centers view around cursor - add sensible toogle behaviour to neo tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -16,14 +16,90 @@ spec =
 }
 
 function setupKeyMaps()
-	vim.keymap.set("n", "\\", ":Neotree filesystem reveal left<CR>")
+	vim.keymap.set('n', '\\', toogleNeoTree)
 end
+
+
 function replaceNetrw()
---	vim.g.loaded_netrw = 1
---	vim.g.loaded_netrwPlugin = 1
---	TODO: add code here, that open neotree on startup
---	if the argument given is a folder.
---	Also make sure to copy update CWD from the other repo
+
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
+	-- Initialize startup_dir to nil
+	local startup_dir = nil
+
+	-- Iterate over all command-line arguments to find the first directory
+	for _, arg in ipairs(vim.fn.argv()) do
+	  if vim.fn.isdirectory(arg) == 1 then
+	    startup_dir = arg
+	    break
+	  end
+	end
+
+	-- Use this directory with Neo-tree
+	source_selector = {
+	 -- Set initial root to the directory we found
+	  default_source = startup_dir,
+	}
+
+	if startup_dir ~= nil then
+		-- add code to show neotree here
+	end
+
+	-- TODO: add code here, that open neotree on startup
+	-- if the argument given is a folder.
+	-- Also make sure to copy update CWD from the other repo
+	
+	-- if vim was called with an argument specifying a directory, make that directory the CWD
+	-- taken from: https://www.reddit.com/r/neovim/comments/1du176f/how_to_align_cwd_with_opened_folder/
+	vim.api.nvim_create_autocmd('VimEnter', {
+	  pattern = '*',
+	  callback = function()
+	    local from = vim.uv.cwd()
+	    local target
+	    local args = vim.fn.argv()
+	    if type(args) ~= 'table' then
+		    return    
+	    end
+
+	    for _, arg in ipairs(type(args) == 'table' and args or {}) do
+	      if vim.fn.isdirectory(arg) then
+		target = vim.fn.fnamemodify(arg, ':p')
+		print(string.format('Changing CWD from %s to %s', from, arg))
+
+		if target ~= nil and vim.fn.isdirectory(target) ~= 0 then
+		  vim.cmd(string.format(':cd %s', target))
+		  vim.notify(string.format('cd to %s', target))
+		end
+	      end
+	    end
+	    return true
+	  end,
+	})
+
+end
+
+function toogleNeoTree()
+	if isNeoTreeWindowFocused()then
+		hideNeoTree()
+	else
+		focusNeoTree()
+	end
+end
+
+function isNeoTreeWindowFocused()
+	return vim.bo.filetype == 'neo-tree'
+end
+
+function isNeoTreeWindowFocused()
+	return vim.bo.filetype == 'neo-tree'
+end
+
+function hideNeoTree()
+	require('neo-tree.command').execute( {action='close' })
+end
+
+function focusNeoTree()
+	require('neo-tree.command').execute( {action='focus' })
 end
 
 return spec
--- lua/vimNative.lua
+++ lua/vimNative.lua
@@ -9,14 +9,18 @@ vim.opt.splitbelow = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.scrolloff = 7
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- split window navigation
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- make half page scrolling always have 
+-- cursor in the middel of the page if possible
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {desc = 'Move half page up and center view around cursor'})
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {desc = 'Move half page down and center view around cursor'})
 
--- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -24,4 +28,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
