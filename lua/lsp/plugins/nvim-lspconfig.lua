0f5e56bc387b9dbe96ff77cb2bedc8eb6d8e20f3
Joshua Krieger <joshua.krieger@stud.th-luebeck.de>
2025-05-03 17:55:09 +0200

    - restructure config, make lps's more modular

--- lua/lsp/plugins/nvim-lspconfig.lua
+++ lua/lsp/plugins/nvim-lspconfig.lua
@@ -4,12 +4,11 @@ return {
 		--print("Loading lsps....")
 		-- snippet of code to iterate the servers folder and load all lsp server init code files
 		local lualspRelativePath = "lua/lsp/servers"
-		for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/"..lualspRelativePath)) do
+		local lualspLoadPath = "lsp/servers"
+		for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/" .. lualspRelativePath)) do
 			local lspServerFile = string.match(file, "(%g*)%.%g")
-			local fileToLoad = lualspRelativePath .. "/" .. lspServerFile
-			print(fileToLoad)
+			local fileToLoad = lualspLoadPath .. "/" .. lspServerFile
 			require(fileToLoad)
 		end
-		--require("lua/lsp/servers/lua_ls")
 	end,
 }
