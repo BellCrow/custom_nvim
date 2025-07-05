df37058f1a7bf751a5a02974c3b5cadb8e136fa5
Joshua Krieger <joshua.krieger@stud.th-luebeck.de>
2025-07-05 15:06:13 +0200

    - make current line more visible

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
 	"folke/tokyonight.nvim",
-	opts = {},
-	config = function()
+	init = function()
 		vim.cmd.colorscheme("tokyonight-storm")
 	end,
+	opts = {
+		transparent = true,
+		on_highlights = function(highlights, colors)
+			-- this makes the current line a bit more bright
+			highlights.CursorLine = { bg = "#333952" }
+		end,
+	},
 }
