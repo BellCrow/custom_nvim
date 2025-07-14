9cc0bc307e0a207ca77e5f93387a1ffe6aa0706c
Joshua Krieger <joshua.krieger@stud.th-luebeck.de>
2025-07-14 23:38:44 +0200

    - add lsp support for gitlab ci yaml

--- lua/lsp/servers/yamlls.lua
+++ lua/lsp/servers/yamlls.lua
@@ -4,7 +4,9 @@ lspconfig.yamlls.setup({
 		yaml = {
 			schemas = {
 				-- makes sure to always use the kubernetes schmema if the file names end with ".k8s.yaml"
-				kubernetes = "/*.k8s.yaml",
+				kubernetes = "/*.k8s.{yaml, yml}",
+				["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "gitlab-ci.{yaml, yml}"
+
 			},
 		},
 	},
